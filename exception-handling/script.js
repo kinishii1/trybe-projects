e3511ca1093e5f7b1b5e05ff1610f0d4d645a524
Kin Ishii <kinishii97@gmail.com>
2023-09-16 02:26:41 +0900

    exception handling

---
+++ exception-handling/script.js
@@ -0,0 +1,37 @@
+const validateNumber = (value1, value2) => {
+  if (!value1 || !value2) {
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+
+  if (Number.isNaN(Number(value1)) || Number.isNaN(Number(value2))) {
+    throw new Error('Informe dois números para realizar a soma');
+  }
+};
+
+function calculateSum() {
+  try {
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+    validateNumber(value1, value2);
+    const result = Number(value1) + Number(value2);
+    return result;
+  } catch (error) {
+    document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+  }
+}
+
+function displayResult(result) {
+  document.getElementById('result').innerHTML = result;
+}
+
+function sum() {
+  const result = calculateSum();
+  if (result) displayResult(result);
+  document.getElementById('value1').value = '';
+  document.getElementById('value2').value = '';
+}
+
+window.onload = () => {
+  const button = document.getElementById('button');
+  button.addEventListener('click', sum);
+};
